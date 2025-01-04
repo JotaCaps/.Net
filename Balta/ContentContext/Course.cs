3290cf35d3aa47bd7b37f64688103a031e4ffb82
João Sérgio <sergiojoao494@gmail.com>
2025-01-04 18:02:50 -0300

    att

--- Balta/ContentContext/Course.cs
+++ Balta/ContentContext/Course.cs
@@ -5,29 +5,11 @@ namespace Balta.ContentContext
     public class Course : Content
     {
         public string Tag { get; set; }
-        public IList<Modules> Modules { get; set; }
+        public IList<Module> Modules { get; set; }
 
         public Course()
         {
-            Modules = new List<Modules>();
+            Modules = new List<Module>();
         }
-    }
-
-    public class Modules
-    {
-        public int Order { get; set; }
-        public string Title { get; set; }
-        public IList<Lecture> Lectures { get; set; }
-
-        public Modules()
-        {
-            Lectures = new List<Lecture>();
-        }
-    } 
-
-    public class Lecture
-    {
-        public int Ordem { get; set; }
-        public string Title { get; set; }
     }
 }
---
+++ Balta/ContentContext/Lecture.cs
@@ -0,0 +1,6 @@
+namespace Balta.ContentContext;
+    public class Lecture
+    {
+        public int Ordem { get; set; }
+        public string Title { get; set; }
+    }
