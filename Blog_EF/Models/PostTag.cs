e65135ae1116546aa2a6254383c370fc240ac3c7
João Sérgio <sergiojoao494@gmail.com>
2025-01-30 16:44:47 -0300

    att

---
+++ Blog_EF/Models/PostTag.cs
@@ -0,0 +1,9 @@
+namespace Blog_EF
+{
+    public class PostTag
+    {
+        public int PostId { get; set; }
+        public int TagId { get; set; }
+
+    }
+}
---
+++ Blog_EF/Models/UserRole.cs
@@ -0,0 +1,8 @@
+namespace Blog_EF
+{
+    public class UserRole
+    {
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+    }
+}
