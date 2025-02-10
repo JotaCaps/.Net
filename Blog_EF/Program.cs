9c52c559764be767988ad074c651f2e7f09a42f7
João Sérgio <sergiojoao494@gmail.com>
2025-02-10 14:50:43 -0300

    att

--- Blog_EF/Program.cs
+++ Blog_EF/Program.cs
@@ -8,15 +8,5 @@ internal class Program
     private static void Main(string[] args)
     {
         using var context = new BlogDataContext();
-
-        context.Users.Add(new User{
-            Bio = "9x Microsoft MVP",
-            Email = "andre@balta.com",
-            Image = "https://balta.io",
-            Name = "André Baltieri",
-            PassWordHash = "1234",
-            Slug = "andre-baltieri"
-        });
-        context.SaveChanges();
     }
 }
--- Blog_EF/obj/Debug/net8.0/Blog_EF.AssemblyInfo.cs
+++ Blog_EF/obj/Debug/net8.0/Blog_EF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog_EF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc561399f6516c88cb0a8ff609a95f7dbdf269de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+209eee2a3388658e94c4727f46f41d01304285d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog_EF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog_EF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
