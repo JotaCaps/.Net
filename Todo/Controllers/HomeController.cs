3632adae4e1c30966ad0a7f50b3f414c85d13e52
João Sérgio <sergiojoao494@gmail.com>
2025-02-12 16:04:14 -0300

    att

--- Todo/Controllers/HomeController.cs
+++ Todo/Controllers/HomeController.cs
@@ -32,27 +32,31 @@ namespace Todo.Conrolers
         }
 
         [HttpPut("/{id:int}")]
-        public TodoModel Put([FromRoute] int id, [FromBody] TodoModel todo, [FromServices]AppDbContext context)
+        public IActionResult Put([FromRoute] int id, [FromBody] TodoModel todo, [FromServices]AppDbContext context)
         {
             var model = context.Todos.FirstOrDefault(x => x.Id == id);
             if(model == null)
-                return todo;
+                return NotFound();
 
             model.Title = todo.Title;
             model.Done = todo.Done;
 
             context.Todos.Update(model);
             context.SaveChanges();
-            return model;   
+            return Ok(model);   
         }
 
         [HttpDelete("/{id:int}")]
-        public TodoModel Delete([FromRoute] int id, [FromServices]AppDbContext context)
+        public IActionResult Delete([FromRoute] int id, [FromServices]AppDbContext context)
         {
             var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            if(model == null)
+                return NotFound();
+                
             context.Todos.Remove(model);
             context.SaveChanges();
-            return model;   
+            
+            return Ok(model);   
         }
     }
 }
