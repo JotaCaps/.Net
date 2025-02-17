36cc9acda670f1c0a2efc29ded3d63362fd5a9a3
João Sérgio <sergiojoao494@gmail.com>
2025-02-17 17:11:16 -0300

    att

---
+++ Blog3/Services/TokenService.cs
@@ -0,0 +1,19 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Blog3;
+using Blog3.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Blog.Services;
+
+public class TokenService
+{
+    public string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var tokenDescriptor = new SecurityTokenDescriptor();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
---
+++ Blog3/ViewModels/Configuration.cs
@@ -0,0 +1,9 @@
+namespace Blog3
+{
+    public static class Configuration
+    {
+        // TOKEN - JWT - Json Web Token
+        public static string JwtKey { get; set; } = "fj399fmaslsdjfnddeaithvclvcne";
+
+    }
+}
